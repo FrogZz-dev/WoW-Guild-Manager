ab5e465b2fcf73f6aa7dfd81ccf5bafb5d042140
laurentdelay <laurent.delay89@gmail.com>
2020-11-15 23:35:32 +0100

    ajout d'une carte Work in Progress

---
+++ src/components/gestion/groups-manager/GroupsCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Card, Spinner } from "react-bootstrap";
+import WorkInProgress from "@components/misc/WorkInProgress";
+
+export default function GroupsCard() {
+  return <WorkInProgress />;
+}
---
+++ src/components/misc/WorkInProgress.js
@@ -0,0 +1,34 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faExclamationTriangle,
+  faTools,
+} from "@fortawesome/free-solid-svg-icons";
+import React from "react";
+import { Card, Spinner } from "react-bootstrap";
+
+export default function WorkInProgress() {
+  return (
+    <Card bg="dark" border="warning" text="warning">
+      <Card.Body className="text-center">
+        <Card.Title style={{ fontSize: "2.5rem" }}>
+          <FontAwesomeIcon
+            icon={faExclamationTriangle}
+            className="text-danger mr-2"
+          />
+          Travaux en cours !
+          <FontAwesomeIcon
+            icon={faExclamationTriangle}
+            className="text-danger ml-2"
+          />
+        </Card.Title>
+        <Card.Body>
+          <FontAwesomeIcon
+            className="text-warning"
+            style={{ fontSize: "5rem" }}
+            icon={faTools}
+          />
+        </Card.Body>
+      </Card.Body>
+    </Card>
+  );
+}
